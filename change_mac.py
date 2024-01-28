a20887b32543a85d2279031e7b41cad3649d974c
RDM-Rux <rulexeyev@gmail.com>
2024-01-28 19:31:07 +0100

    Parse user input, implemented interface and mac changer functionality

--- change_mac.py
+++ change_mac.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
-import subprocess
 
-subprocess.call("ifconfig", Shell=True)
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "up"])
