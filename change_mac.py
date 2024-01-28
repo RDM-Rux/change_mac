8dbe76f7e238c3a5a21348a568acdd9956da305f
RDM-Rux <rulexeyev@gmail.com>
2024-01-28 19:55:41 +0100

    Refactoring

--- change_mac.py
+++ change_mac.py
@@ -3,6 +3,11 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -12,11 +17,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-
-(options, arguments) = parser.parse_args()
-
+(options, arguments) = get_arguments()
 change_mac(options.interface, options.new_mac)
